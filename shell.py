af953e0004d289b760785ff31b2becbdd133420f
gregorop <gregorop@yandex.ru>
2025-07-13 22:24:46 +0300

    локальная щель в контейнер с бд

--- shell.py
+++ shell.py
@@ -13,45 +13,48 @@ from sqlalchemy import (
     text,
     update,
 )
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import (
     aliased,
     load_only,
+    sessionmaker,
 )
 
-from app.core.database import AsyncSessionLocal, engine
 from app.models.base import DBTask
+from settings import LOCAL_SQLALCHEMY_DATABASE_URL
+
+engine = create_async_engine(LOCAL_SQLALCHEMY_DATABASE_URL)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def async_shell():
     session = AsyncSessionLocal()
-    
-    namespace = {
-        'session': session,
-        'DBTask': DBTask,
-        
-        'select': select,
-        'insert': insert,
-        'update': update,
-        'delete': delete,
-        
-        'and_': and_,
-        'or_': or_,
-        'not_': not_,
-        'text': text,
-        'asc': asc,
-        'desc': desc,
 
-        'load_only': load_only,
-        'aliased': aliased,
+    namespace = {
+        "session": session,
+        "DBTask": DBTask,
+        "select": select,
+        "insert": insert,
+        "update": update,
+        "delete": delete,
+        "and_": and_,
+        "or_": or_,
+        "not_": not_,
+        "text": text,
+        "asc": asc,
+        "desc": desc,
+        "load_only": load_only,
+        "aliased": aliased,
     }
 
     async def wrapper():
-        await interact(banner="мой shell c awaitom и моделями",locals=namespace)
-    
+        await interact(banner="мой shell c awaitom и моделями", locals=namespace)
+
     try:
         await wrapper()
     finally:
         await session.close()
         await engine.dispose()
+
 
 asyncio.run(async_shell())
