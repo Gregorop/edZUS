d40aa8c72d4d2a4b455014d47a7f3dd4624409b8
gregorop <gregorop@yandex.ru>
2025-07-13 14:55:58 +0300

    тест на patch прошел после мержа

--- app/tests/test_one_action_formula_web.py
+++ app/tests/test_one_action_formula_web.py
@@ -35,3 +35,37 @@ async def test_read_formulas(async_client, session):
     data = response.json()
     assert len(data) == 3
     assert data[0]["formula"] == "m*10*h"
+
+
+@pytest.mark.asyncio
+async def test_update_formula(async_client, session):
+    create_response = await async_client.post(
+        "/one_action_formula/",
+        json={
+            "formula": "m*10*h",
+            "question": "Тело массой {m} кг с высоты {h}....",
+            "param_ranges": {"m": [1, 100], "h": [1, 100]},
+        },
+    )
+    assert create_response.status_code == 200
+    created_data = create_response.json()
+    formula_id = created_data["id"]
+
+    update_response = await async_client.patch(
+        f"/one_action_formula/{formula_id}",
+        json={
+            "formula": "m*9.8*h",
+        },
+    )
+    assert update_response.status_code == 200
+    updated_data = update_response.json()
+
+    assert updated_data["formula"] == "m*9.8*h"
+    assert updated_data["param_ranges"] == {"m": [1, 100], "h": [1, 100]}
+
+    result = await session.execute(
+        select(OneActionFormula).where(OneActionFormula.id == formula_id)
+    )
+
+    db_formula = result.scalars().first()
+    assert db_formula.formula == "m*9.8*h"
